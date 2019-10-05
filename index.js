6320767ef334f81c4e2fa2dd5445fbe1f07bc7db
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-05 10:44:36 -0700

    return number in 2 decimal digits

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+// receive value in cents and return in R$1.000,00 format
 const currencyFormat = value => {
-	if (value) return value
+	if (value.match(/^0[0-9]{0,1}$|^[1-9]([0-9]?)+$/g)) {
+		return (parseInt(value, 10)/100).toFixed(2)
+	}
 	return ''
 }
 
