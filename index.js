59a95aa86174313ff966a7577daa94cb2516668b
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-05 10:50:11 -0700

    checks if values are allowed

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // receive value in cents and return in R$1.000,00 format
 const currencyFormat = value => {
-	if (value.match(/^0[0-9]{0,1}$|^[1-9]([0-9]?)+$/g)) {
-		return (parseInt(value, 10)/100).toFixed(2)
+	if (value) {
+		const valueString = value.toString()
+		// checks if string is in integer format and if it starts with 0 it only has 2 digits
+		if (valueString.match(/^0[0-9]{0,1}$|^[1-9]([0-9]?)+$/g)) {
+			return (parseInt(value, 10)/100).toFixed(2)
+		}
 	}
 	return ''
 }
