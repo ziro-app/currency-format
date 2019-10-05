5f95928f3d5fd3cbe6b0897399e7094de7bd24b7
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-10-05 11:30:35 -0700

    case for when value is below the thousands

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // receive value in cents and return in R$1.000,00 format
 const currencyFormat = value => {
 	if (value) {
-		const valueString = value.toString()
+		const valueString = `${value}`
 		// checks if string is in integer format and if it starts with 0 it only has 2 digits
 		if (valueString.match(/^0[0-9]{0,1}$|^[1-9]([0-9]?)+$/g)) {
-			return (parseInt(value, 10)/100).toFixed(2)
+			const noFormat = (parseInt(value, 10)/100).toFixed(2)
+			if (noFormat.length <= '6')
+				return `R$${noFormat.replace('.',',')}`
 		}
 	}
 	return ''
