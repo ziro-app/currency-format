fe414118bfebce0e9271044399ee3d25cea6ec4e
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2020-04-24 18:39:20 -0700

    add 'use strict' and allow integer 0 as valid input

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// receive value in cents and return in R$1.000,00 format, does not format values above R$999.999,99
-const currencyFormat = value => {
+'use strict' // throws if octal literals are passed as parameters to the function
+// this function receives a value in cents and returns it in R$1.000,00 format
+// does not format values above R$999.999,99
+const currencyFormat = receivedValue => {
+	const value = receivedValue === 0 ? '00' : receivedValue
 	if (value) {
 		const valueString = `${value}`
-		// checks if string is in integer format and if it starts with 0 it only has 2 digits
+		// checks if string is in integer format and if it only has 2 digits if starts with leading 0
 		if (valueString.match(/^0[0-9]{0,1}$|^[1-9]([0-9]?)+$/g)) {
 			const noFormat = (parseInt(value, 10)/100).toFixed(2)
 			if (noFormat.length <= '6')
